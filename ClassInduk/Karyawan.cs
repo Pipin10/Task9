f9301424565867699260a9f7db43e8e78d7e6e49
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-06-29 23:56:22 +0700

    Prog CRUD

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasLab9.ClassInduk
+{
+    public abstract class Karyawan
+    {
+        public string NIK;
+        public string Nama;
+        public string Jenis;
+        public abstract double Gaji();
+    }
+}
